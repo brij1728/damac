a4121401b8e547a6ff8c6c5f5ce40fa8f4b5feaa
Brijesh Kumar <bk10895@gmail.com>
2024-06-20 00:02:13 +0530

    feat: enhance Hero component and PriceCard UI
    - Modify Hero container styling for improved layout and responsiveness
    - Add new text sections to Hero for additional information display
    - Refactor Hero and PriceCard text styling for better readability
    - Update PriceCard layout and information presentation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export const Hero = () => {
   return (
-    <div className="relative flex h-screen w-full flex-col pl-4 pr-6">
+    <div className="relative flex h-screen w-full flex-col pb-10 pl-4 pr-6">
       <Image
         src="/hero.jpg"
         alt="Hero background"
@@ -14,20 +14,28 @@ export const Hero = () => {
         className="absolute inset-0 z-[-1]"
       />
       <Header />
-      <div className="mt-6 flex sm:flex-row sm:justify-between">
+      <div className="mt-6 flex flex-col sm:flex-row sm:justify-between">
         <div>
-          <p className="font-oswald text-[44px] font-medium uppercase text-secondary-300">
+          <p className="font-oswald text-[37px] font-medium uppercase text-secondary-300 sm:text-[44px]">
             Harbour Lights <br />
             de<span className="text-primary-100"> GRESOGONO</span>
           </p>
-          <p className="text mt-4 font-poppins text-[25px] font-bold text-secondary-300">
+          <p className="mt-4 font-poppins text-[14px] font-bold text-secondary-300 sm:text-[25px]">
             1, 2 & 3 Bedrooms Seaside Apartments <br />
             in Dubai Maritime City
           </p>
+          <div className="mt-10 flex flex-col gap-4 font-poppins text-[14px] text-secondary-100">
+            <p className="max-w-sm border-l-4 border-primary-100 bg-black/25 p-2">
+              Rental Returns of{' '}
+              <span className="text-base font-bold">UPTO 11%**</span>
+            </p>
+            <p className="max-w-sm border-l-4 border-primary-100 bg-black/25 p-2">
+              Capital Appreciation of{' '}
+              <span className="text-base font-bold">UPTO 32%**</span>
+            </p>
+          </div>
         </div>
-        <div>
-          <PriceCard />
-        </div>
+        <PriceCard />
       </div>
     </div>
   );
--- src/components/ui/PriceCard.tsx
+++ src/components/ui/PriceCard.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 
 export const PriceCard = () => {
   return (
-    <div className="backdrop-blur-4xl flex flex-col items-start justify-center gap-2 rounded bg-primary-400/65 px-8 py-4 font-oswald">
-      <div className="text-secondary-300">
-        <p className="text-[13px] uppercase">Pricing starting from</p>
-        <p className="text-4xl font-medium">$ 425,000</p>
-        <p className="text-lg font-normal">AED 1.3 Million</p>
-        <button className="mt-4 rounded bg-primary-100 px-10 py-3 text-[13px] font-bold uppercase text-white">
-          Get a presentation
-        </button>
+    <div className="flex flex-col items-start justify-start gap-8 rounded-[14px] bg-primary-400/10 px-8 pt-8 backdrop-blur">
+      <div className="gap-3 text-secondary-300">
+        <p className="font-poppins text-[13px] font-normal uppercase">
+          Pricing starting from
+        </p>
+        <p className="font-oswald text-5xl font-medium">$ 425,000</p>
+        <p className="font-oswald text-2xl font-normal">AED 1.3 Million</p>
       </div>
-      <footer className="pt-5">
+      <button className="rounded bg-primary-100 px-10 py-3 font-poppins text-[13px] font-bold uppercase text-white">
+        Get a presentation
+      </button>
+      <div>
         <p className="font-poppins text-[13px] text-primary-500">
           Get an Expert’s Presentation of Real <br />
           Estate in Dubai for Life and investment
         </p>
-      </footer>
+      </div>
     </div>
   );
 };
