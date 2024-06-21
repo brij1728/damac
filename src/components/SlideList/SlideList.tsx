22319d9145f6c92844bef0ce4fba2f68c9009edf
Brijesh Kumar <bk10895@gmail.com>
2024-06-21 15:11:19 +0530

    style: adjust prettier configuration and update SlideList component style
    - Remove prettier-plugin-tailwindcss from .prettierrc
    - Modify padding and gap in SlideList component to improve layout and responsiveness

--- src/components/SlideList/SlideList.tsx
+++ src/components/SlideList/SlideList.tsx
@@ -5,7 +5,7 @@ import { Slider } from '../ui';
 export const SlideList = () => {
   return (
     <>
-      <div className="bg-secondary-400 flex items-center justify-center gap-10 sm:gap-20">
+      <div className="px-814 bg-secondary-400 flex w-full items-center justify-center gap-6 sm:gap-16">
         {Items.map((item, index) => (
           <div key={index}>
             <Slider
