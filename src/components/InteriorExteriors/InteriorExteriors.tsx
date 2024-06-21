993c9a279644bd48ebb75b0fd7ca643e0e6ddad0
Brijesh Kumar <bk10895@gmail.com>
2024-06-21 22:04:32 +0530

    feat: add interactivity to InteriorExteriors component
    - Add state to InteriorExteriors component to toggle between interiors and exteriors views
    - Update Button component to accept onClick prop for increased interactivity
    - Implement conditional rendering in InteriorExteriors based on state

--- src/components/InteriorExteriors/InteriorExteriors.tsx
+++ src/components/InteriorExteriors/InteriorExteriors.tsx
@@ -1,64 +1,82 @@
+'use client';
+
+import React, { useState } from 'react';
+
 import { Button } from '../ui';
 import Image from 'next/image';
-import React from 'react';
 
 export const InteriorExteriors = () => {
+  const [showInteriors, setShowInteriors] = useState(false);
+
   return (
     <div className="flex flex-col justify-center items-center p-4 w-full mx-auto lg:w-10/12">
       <div className="flex justify-end gap-2 w-full my-5">
         <Button
           text="Exteriors"
-          classNames="text-secondary-300 bg-primary-200 border-secondary-300"
+          classNames={`${
+            !showInteriors
+              ? 'text-secondary-300 bg-primary-200 border-secondary-300'
+              : 'text-primary-200 border-primary-200 border-[0.5px]'
+          }`}
+          onClick={() => setShowInteriors(false)}
         />
         <Button
           text="Interiors"
-          classNames="text-primary-200 border-primary-200 border-[0.5px]"
+          classNames={`${
+            showInteriors
+              ? 'text-secondary-300 bg-primary-200 border-secondary-300'
+              : 'text-primary-200 border-primary-200 border-[0.5px]'
+          }`}
+          onClick={() => setShowInteriors(true)}
         />
       </div>
       <div className="w-full ">
-        <div className="mb-4 w-full flex justify-center">
-          <Image
-            src="/exteriors.png"
-            alt="Exterior"
-            width={1120}
-            height={500}
-            layout="responsive"
-          />
-        </div>
-        <div className="flex justify-center gap-1 sm:gap-4">
-          <Image
-            src="/interiors1.jpg"
-            alt="Interior1"
-            width={265}
-            height={170}
-            className="object-cover"
-            layout="responsive"
-          />
-          <Image
-            src="/interiors2.jpg"
-            alt="Interior2"
-            width={265}
-            height={170}
-            className="object-cover"
-            layout="responsive"
-          />
-          <Image
-            src="/interiors3.jpg"
-            alt="Interior3"
-            width={265}
-            height={170}
-            className="object-cover"
-            layout="responsive"
-          />
-          <Image
-            src="/interiors4.jpg"
-            alt="Interior4"
-            width={265}
-            height={170}
-            className="object-cover"
-            layout="responsive"
-          />
-        </div>
+        {!showInteriors ? (
+          <div className="mb-4 w-full flex justify-center">
+            <Image
+              src="/exteriors.png"
+              alt="Exterior"
+              width={1120}
+              height={500}
+              layout="responsive"
+            />
+          </div>
+        ) : (
+          <div className="flex justify-center gap-1 sm:gap-4">
+            <Image
+              src="/interiors1.jpg"
+              alt="Interior1"
+              width={265}
+              height={170}
+              className="object-cover"
+              layout="responsive"
+            />
+            <Image
+              src="/interiors2.jpg"
+              alt="Interior2"
+              width={265}
+              height={170}
+              className="object-cover"
+              layout="responsive"
+            />
+            <Image
+              src="/interiors3.jpg"
+              alt="Interior3"
+              width={265}
+              height={170}
+              className="object-cover"
+              layout="responsive"
+            />
+            <Image
+              src="/interiors4.jpg"
+              alt="Interior4"
+              width={265}
+              height={170}
+              className="object-cover"
+              layout="responsive"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 interface ButtonProps {
   text: string;
   classNames?: string;
+  onClick?: () => void;
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, classNames = '' }) => {
+export const Button: React.FC<ButtonProps> = ({
+  text,
+  onClick,
+  classNames = '',
+}) => {
   return (
     <button
+      onClick={onClick}
       className={`rounded-md border-2 border-primary-100 px-11 py-3 font-poppins text-[13px] font-bold text-primary-100 uppercase ${classNames}`}
     >
       {text}
